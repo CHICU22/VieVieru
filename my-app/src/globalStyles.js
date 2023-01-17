a5c287dde29e7c45eace415ecf3d68793907aa94
CHICU22 <alexandre_vieru@eticalgarve.com>
2023-01-17 17:21:46 +0000

    Created and added some css to navbar

---
+++ my-app/src/globalStyles.js
@@ -0,0 +1,15 @@
+import styled, {createdGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createdGlobalStyle`
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Source Sans Pro', sans-seriff;
+}
+`;
+
+export const Container = styled.div`
+z-index: 1;
+width
+`
